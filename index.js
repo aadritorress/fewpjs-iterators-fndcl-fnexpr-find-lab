3c461ebffcafc25f52c8327fe926b79c331b3952
Adriana <adriana.amaral@msn.com>
2021-04-16 21:46:53 -0500

    commit tree 824c6b9544fedcb82d974ada35c58d0962ee608e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+const superbowlWin = (record) => {
+return record.find( function(s) { return s == "W" })
+}
+
+// superbowlWin = (record) => {
+//     let result = record.find( record => record.result === "W" )
+//     return !!result ? result.year :undefined
+//   }
